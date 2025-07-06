ac55478492b2330a4d89d8ef5d105e142a452e3e
Jana1902 <jana7cr@gmail.com>
2025-07-06 18:31:54 +0530

    Updated API URL

--- src/axio.js
+++ src/axio.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001",
+  baseURL: "https://api-headline-creator.onrender.com",
   withCredentials: true,
 });
